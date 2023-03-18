3a4bdcfba1e97cf2e6a28c959f0a15acb3da7c6a
mauroaicode <mauricioaicode2310@gmail.coml>
2023-03-18 08:27:55 -0500

    ajuste para desplegar en producción

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,11 @@ module.exports = {
     {
       name: 'SaludWom-Admin',
       script: './start.js',
+      port: 800,
       env:{
         HOST: 'localhost',
         PORT: 8000
       }
     },
-    {
-      name: 'admin-saludwom.test-aicode.com',
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
-      env:{
-        HOST: 'localhost',
-        PORT: 8000
-      }
-    }
   ]
 }
