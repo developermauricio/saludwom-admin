95833977be8040d73dce84d6bd6af611730f3ccb
mauroaicode <mauricioaicode2310@gmail.coml>
2023-03-18 08:34:48 -0500

    ajuste para desplegar en producción

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'SaludWom-Admin',
       script: './start.js',
-      port: 800,
+      port: 8000,
       env:{
         HOST: 'localhost',
         PORT: 8000
